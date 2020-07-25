92b3657b4a2c202a1a9196e2a140c43b2ab3ac2e
Timo Tijhof <krinklemail@gmail.com>
2020-07-25 21:15:47 +0100

    tests: Convert data provider to use labelled generator
    This also makes the labels more descriptive. e.g. instead of
    stating that test case  6 failed, it will refer to case "total flattening".
    This also helps with interpreting of the code coverage report.
    
    Change-Id: Idec5aa92221c79a06d446b28fe4ea947024a367f

--- tests/HtmlFormatterTest.php
+++ tests/HtmlFormatterTest.php
@@ -11,8 +11,7 @@ use HtmlFormatter\HtmlFormatter;
 class HtmlFormatterTest extends \PHPUnit\Framework\TestCase {
 
 	/**
-	 * @dataProvider getHtmlData
-	 *
+	 * @dataProvider provideHtmlData
 	 * @param string $input
 	 * @param string $expectedText
 	 * @param array $expectedRemoved
@@ -52,7 +51,7 @@ class HtmlFormatterTest extends \PHPUnit\Framework\TestCase {
 		);
 	}
 
-	public function getHtmlData() {
+	public static function provideHtmlData() {
 		$removeImages = function ( HtmlFormatter $f ) {
 			$f->setRemoveMedia();
 		};
@@ -65,76 +64,67 @@ class HtmlFormatterTest extends \PHPUnit\Framework\TestCase {
 		$flattenEverything = function ( HtmlFormatter $f ) {
 			$f->flattenAllTags();
 		};
-		return [
-			// remove images if asked
-			[
-				'<img src="/foo/bar.jpg" alt="Blah"/>',
-				'',
-				[ '<img src="/foo/bar.jpg" alt="Blah">' ],
-				$removeImages,
-			],
-			// basic tag removal
-			[
-				// @codingStandardsIgnoreStart Ignore long line warnings.
-				'<table><tr><td>foo</td></tr></table><div class="foo">foo</div><div class="foo quux">foo</div><span id="bar">bar</span>
+		yield 'remove images' => [
+			'<img src="/foo/bar.jpg" alt="Blah"/>',
+			'',
+			[ '<img src="/foo/bar.jpg" alt="Blah">' ],
+			$removeImages,
+		];
+		yield 'basic tag removal' => [
+			// @codingStandardsIgnoreStart Ignore long line warnings.
+			'<table><tr><td>foo</td></tr></table><div class="foo">foo</div><div class="foo quux">foo</div><span id="bar">bar</span>
 <strong class="foo" id="bar">foobar</strong><div class="notfoo">test</div><div class="baz"/>
 <span class="baz">baz</span>',
-				// @codingStandardsIgnoreEnd
-				'<div class="notfoo">test</div>
+			// @codingStandardsIgnoreEnd
+			'<div class="notfoo">test</div>
 <span class="baz">baz</span>',
-				[
-					'<table><tr><td>foo</td></tr></table>',
-					'<div class="foo">foo</div>',
-					'<div class="foo quux">foo</div>',
-					'<span id="bar">bar</span>',
-					'<strong class="foo" id="bar">foobar</strong>',
-					'<div class="baz"/>',
-				],
-				$removeTags,
-			],
-			// selector ".foo" should not match ".telefoon" or ".nofoo"
 			[
-				'<div class="telefoon">A</div><div class="nofoo">B</div><div class="foo">C</div>',
-				'<div class="telefoon">A</div><div class="nofoo">B</div>',
-				[],
-				$removeTags,
+				'<table><tr><td>foo</td></tr></table>',
+				'<div class="foo">foo</div>',
+				'<div class="foo quux">foo</div>',
+				'<span id="bar">bar</span>',
+				'<strong class="foo" id="bar">foobar</strong>',
+				'<div class="baz"/>',
 			],
-			// selector ".foo" should not match ".no-foo" or ".foo-bar" (T231160)
-			[
-				'<div class="no-foo">A</div><div class="foo-bar">B</div><div class="foo foo-bar">C</div>',
-				'<div class="no-foo">A</div><div class="foo-bar">B</div>',
-				[],
-				$removeTags,
-			],
-			// don't flatten tags that start like chosen ones
-			[
-				'<div><s>foo</s> <span>bar</span></div>',
-				'foo <span>bar</span>',
-				[],
-				$flattenSomeStuff,
-			],
-			// total flattening
-			[
-				'<div style="foo">bar<sup>2</sup></div>',
-				'bar2',
-				[],
-				$flattenEverything,
-			],
-			// UTF-8 preservation and security
-			[
-				'<span title="&quot; \' &amp;">&lt;Тест!&gt;</span> &amp;&lt;&#38;&#0038;&#x26;&#x026;',
-				'<span title="&quot; \' &amp;">&lt;Тест!&gt;</span> &amp;&lt;&amp;&amp;&amp;&amp;',
-				[],
-				// Have some rules to trigger a DOM parse
-				$removeTags,
-			],
-			// https://phabricator.wikimedia.org/T55086
-			[
-				'Foo<sup id="cite_ref-1" class="reference"><a href="#cite_note-1">[1]</a></sup>'
-					. ' <a href="/wiki/Bar" title="Bar" class="mw-redirect">Bar</a>',
-				'Foo<sup id="cite_ref-1" class="reference"><a href="#cite_note-1">[1]</a></sup>'
-					. ' <a href="/wiki/Bar" title="Bar" class="mw-redirect">Bar</a>',
-			],
+			$removeTags,
+		];
+		yield 'selector ".foo" should not match ".telefoon" or ".nofoo"' => [
+			'<div class="telefoon">A</div><div class="nofoo">B</div><div class="foo">C</div>',
+			'<div class="telefoon">A</div><div class="nofoo">B</div>',
+			[],
+			$removeTags,
+		];
+		yield 'selector ".foo" should not match ".no-foo" or ".foo-bar" (T231160)' => [
+			'<div class="no-foo">A</div><div class="foo-bar">B</div><div class="foo foo-bar">C</div>',
+			'<div class="no-foo">A</div><div class="foo-bar">B</div>',
+			[],
+			$removeTags,
+		];
+		yield 'do not flatten tags that start like chosen ones' => [
+			'<div><s>foo</s> <span>bar</span></div>',
+			'foo <span>bar</span>',
+			[],
+			$flattenSomeStuff,
+		];
+		yield 'total flattening' => [
+			'<div style="foo">bar<sup>2</sup></div>',
+			'bar2',
+			[],
+			$flattenEverything,
+		];
+		yield 'UTF-8 preservation and security' => [
+			'<span title="&quot; \' &amp;">&lt;Тест!&gt;</span> &amp;&lt;&#38;&#0038;&#x26;&#x026;',
+			'<span title="&quot; \' &amp;">&lt;Тест!&gt;</span> &amp;&lt;&amp;&amp;&amp;&amp;',
+			[],
+			// Have some rules to trigger a DOM parse
+			$removeTags,
+		];
+		// https://phabricator.wikimedia.org/T55086
+		yield 'T55086' => [
+			'Foo<sup id="cite_ref-1" class="reference"><a href="#cite_note-1">[1]</a></sup>'
+				. ' <a href="/wiki/Bar" title="Bar" class="mw-redirect">Bar</a>',
+			'Foo<sup id="cite_ref-1" class="reference"><a href="#cite_note-1">[1]</a></sup>'
+				. ' <a href="/wiki/Bar" title="Bar" class="mw-redirect">Bar</a>',
 		];
 	}
 
